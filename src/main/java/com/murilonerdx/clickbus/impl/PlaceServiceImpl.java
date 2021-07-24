6c9a9e4cbe8c98c26d54d1ccdfdfbae664f26c54
Murilonerdx <mu-silva@outlook.com>
2021-07-24 12:17:25 -0300

    creating repository implementation, and adding modelmapper in project

---
+++ src/main/java/com/murilonerdx/clickbus/impl/PlaceServiceImpl.java
@@ -0,0 +1,78 @@
+package com.murilonerdx.clickbus.impl;
+
+import com.murilonerdx.clickbus.dto.PlaceDTO;
+import com.murilonerdx.clickbus.entity.Place;
+import com.murilonerdx.clickbus.exception.PlaceNotFound;
+import com.murilonerdx.clickbus.repository.PlaceRepository;
+import com.murilonerdx.clickbus.service.PlaceService;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class PlaceServiceImpl implements PlaceService {
+
+  private final PlaceRepository repository;
+
+  @Override
+  public Place create(PlaceDTO placeDTO) {
+    Place place = toModel(placeDTO);
+    place.setCreatedAt(LocalDateTime.now());
+    place.setUpdatedAt(place.getCreatedAt());
+    return repository.save(place);
+  }
+
+  private Place toModel(PlaceDTO placeDTO) {
+    return new ModelMapper().map(placeDTO, Place.class);
+  }
+
+  @Override
+  public Place update(Long id, PlaceDTO place) throws PlaceNotFound {
+    Place entity = getById(id);
+    if(entity == null){
+      throw new PlaceNotFound("Place not found");
+    }
+    entity.setCity(place.getCity());
+    entity.setSlug(place.getSlug());
+    entity.setUpdatedAt(LocalDateTime.now());
+    entity.setState(place.getState());
+    return repository.save(entity);
+  }
+
+  @Override
+  public void delete(Place place) throws PlaceNotFound{
+    if(place == null | Objects.requireNonNull(place).getId() == null){
+      throw new PlaceNotFound("Place not found");
+    }
+    repository.delete(place);
+  }
+
+  @Override
+  public Place getById(Long id) {
+    return repository.findById(id).orElseThrow(()-> new IllegalArgumentException("Place id cant be null"));
+  }
+
+  @Override
+  public List<PlaceDTO> getAll() {
+    return repository.findAll().stream()
+        .map(x->new ModelMapper().map(x, PlaceDTO.class))
+        .collect(Collectors.toList());
+  }
+
+  @Override
+  public Place getByName(String name) {
+    try{
+      return repository.getByName(name);
+    }catch(IllegalArgumentException e){
+      return null;
+    }
+  }
+
+
+}
---
+++ src/main/java/com/murilonerdx/clickbus/repository/PlaceRepository.java
@@ -0,0 +1,13 @@
+package com.murilonerdx.clickbus.repository;
+
+import com.murilonerdx.clickbus.entity.Place;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlaceRepository extends JpaRepository<Place, Long> {
+  @Query("select p from Place p where p.name LIKE :name")
+  Place getByName(@Param("name") String name);
+}
